4d4463f3414057565b113813dada75f684b33ab0
Elvis Pichou <elvis@MacBook-Pro-de-Pichou.local>
2025-02-14 09:52:25 +0100

    ✨ Feat(Navabar): Add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/layout/navbar";
-import Footer from "@/components/layout/footer";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark mx-5 md:mx-32 flex flex-col`}
       >
         <Navbar />
         <main className="w-full">{children}</main>
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Card } from "../ui/card";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import ScrollLink from "../common/ScrollLink";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Card className="sticky top-3 mt-3 py-3.5 px-8 rounded-full flex justify-between items-center md:w-9/12 self-center">
+      <Link href="/">
+        <h3 className="text-xl font-bold">Curl</h3>
+      </Link>
+      <div className="gap-0 flex">
+        <ScrollLink targetId={"Philosophy"}>
+          <Button variant={"link"}>Notre philosophie</Button>
+        </ScrollLink>
+        <ScrollLink targetId={"MVP"}>
+          <Button variant={"link"}>MVP</Button>
+        </ScrollLink>
+
+        <ScrollLink targetId={"Pricing"}>
+          <Button variant={"link"}>Nos packs</Button>
+        </ScrollLink>
+
+        <ScrollLink targetId={"WhyUs"}>
+          <Button variant={"link"}>Pourquoi nous ?</Button>
+        </ScrollLink>
+
+        <ScrollLink targetId={"FAQ"}>
+          <Button variant={"link"}>FAQ</Button>
+        </ScrollLink>
+      </div>
+      <div className="flex">
+        <Link href={process.env.CONTACT_URL || "/"} target="_blank">
+          <Button>Nous contacter</Button>
+        </Link>
+      </div>
+    </Card>
+  );
 };
 
 export default Navbar;
